b609c76ff1dba168ab2556b202ff5e16cd72a854
yuki <shimazuyuki7@gmail.com>
2023-02-15 11:30:15 +0900

    footer追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,85 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
-import { FOOTER_HEIGHT, MAIN_COLOR } from "utils/constants";
+import {
+  Button,
+  ButtonGroup,
+  Container,
+  Divider,
+  IconButton,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
+import { FOOTER_HEIGHT, MAIN_COLOR, SITE_NAME } from "utils/constants";
+import { Logo } from "./Logo";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <Flex
+    <Container
       as="nav"
+      role="contentinfo"
       bg={MAIN_COLOR}
       color="gray.50"
       h={FOOTER_HEIGHT}
-      padding={{ base: "0.5rem", md: "0.7rem" }}
       mt={{ base: "1rem", md: "2rem" }}
       position="absolute"
+      minW="100%"
       bottom={0}
       left={0}
       right={0}
-    ></Flex>
+    >
+      <Stack
+        spacing="8"
+        direction={{ base: "column", md: "row" }}
+        justify="space-between"
+        py={{ base: "12", md: "16" }}
+      >
+        <Stack spacing={{ base: "6", md: "8" }} align="start">
+          <Logo />
+          <Text color="muted">
+            Referencing the experiences of others can deepen wisdom.
+          </Text>
+        </Stack>
+        <Stack
+          direction={{ base: "column-reverse", md: "column", lg: "row" }}
+          spacing={{ base: "12", md: "8" }}
+        >
+          <Stack direction="row" spacing="8">
+            <Stack spacing="4" minW="36" flex="1">
+              <Text fontSize="sm" fontWeight="semibold" color="subtle">
+                Product
+              </Text>
+              <Stack spacing="3" shouldWrapChildren>
+                <Link href="/same-grade">
+                  <Button variant="link">Other work</Button>
+                </Link>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+      <Divider />
+      <Stack
+        pt="8"
+        pb="12"
+        justify="space-between"
+        direction={{ base: "column-reverse", md: "row" }}
+        align="center"
+      >
+        <Text fontSize="sm" color="subtle">
+          &copy; {new Date().getFullYear()} {SITE_NAME}
+        </Text>
+        <ButtonGroup variant="ghost">
+          <IconButton
+            as="a"
+            href="https://github.com/yshima42/42feedback"
+            aria-label="GitHub"
+            icon={<FaGithub fontSize="1.25rem" />}
+            _hover={{ bg: "gray.700" }}
+          />
+        </ButtonGroup>
+      </Stack>
+    </Container>
   );
 };
 
